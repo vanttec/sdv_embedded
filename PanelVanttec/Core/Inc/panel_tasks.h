ce27a5ce5573f9e45426d606ee887e25c242e412
EdisonAltamirano <43297927+EdisonAltamirano@users.noreply.github.com>
2023-05-11 01:36:40 -0600

    Added panel software embedded

---
+++ PanelVanttec/Core/Inc/panel_tasks.h
@@ -0,0 +1,15 @@
+/*
+ * panel_tasks.h
+ *
+ *  Created on: May 9, 2023
+ *      Author: books
+ */
+
+#ifndef INC_PANEL_TASKS_H_
+#define INC_PANEL_TASKS_H_
+
+void init_panel_task();
+
+
+
+#endif /* INC_PANEL_TASKS_H_ */
