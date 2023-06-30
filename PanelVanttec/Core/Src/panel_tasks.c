d0643b5bd220014038b6f6546ef6e441281d264a
EdisonAltamirano <43297927+EdisonAltamirano@users.noreply.github.com>
2023-06-30 11:38:19 -0600

    changes panel katia

--- PanelVanttec/Core/Src/panel_tasks.c
+++ PanelVanttec/Core/Src/panel_tasks.c
@@ -8,7 +8,7 @@
 #include "vanttec_canlib_tx_task.h"
 #include "vanttec_canlib_rx_task.h"
 #include "ltc4151cms.h"
-#include "vanttec_canlib_generic_ids.h"
+#include "vanttec_sdv_ids.h"
 
 osThreadId_t debugTaskHandle;
 const osThreadAttr_t debugTaskAttributes = {
--- PanelVanttec/Debug/Core/Src/panel_tasks.d
+++ PanelVanttec/Debug/Core/Src/panel_tasks.d
@@ -1,5 +1,4 @@
-Core/Src/panel_tasks.o: ../Core/Src/panel_tasks.c \
- ../Core/Inc/vanttec_sdv_ids.h ../Core/Inc/main.h \
+Core/Src/panel_tasks.o: ../Core/Src/panel_tasks.c ../Core/Inc/main.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
  ../Core/Inc/stm32l4xx_hal_conf.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
@@ -40,8 +39,7 @@ Core/Src/panel_tasks.o: ../Core/Src/panel_tasks.c \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h \
- ../Core/Inc/ltc4151cms.h
-../Core/Inc/vanttec_sdv_ids.h:
+ ../Core/Inc/ltc4151cms.h ../Core/Inc/vanttec_sdv_ids.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
 ../Core/Inc/stm32l4xx_hal_conf.h:
@@ -84,3 +82,4 @@ Core/Src/panel_tasks.o: ../Core/Src/panel_tasks.c \
 ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
 ../Core/Inc/ltc4151cms.h:
+../Core/Inc/vanttec_sdv_ids.h:
