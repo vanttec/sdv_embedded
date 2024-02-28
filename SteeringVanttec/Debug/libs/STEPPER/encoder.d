9fe059aeca1ce133beabf81e818da4fd1b7d6bc3
romi2002 <empoleom@gmail.com>
2024-02-28 17:37:40 -0600

    Removed build files from git

--- SteeringVanttec/Debug/libs/STEPPER/encoder.d
+++
@@ -1,3 +0,0 @@
-libs/STEPPER/encoder.o: ../libs/STEPPER/encoder.c \
- ../libs/STEPPER/encoder.h
-../libs/STEPPER/encoder.h:
--- SteeringVanttec/Debug/libs/stepper_lib/encoder.d
+++
@@ -1,3 +0,0 @@
-libs/stepper_lib/encoder.o: ../libs/stepper_lib/encoder.c \
- ../libs/stepper_lib/encoder.h
-../libs/stepper_lib/encoder.h:
