621d099253f0848346eedeadd98026721edf04ce
saucesaft <eduarch42@protonmail.com>
2024-03-19 17:41:40 -0600

    better motor period

--- SteeringVanttec/Core/Src/main.c
+++ SteeringVanttec/Core/Src/main.c
@@ -494,7 +494,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 80-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_DOWN;
-  htim2.Init.Period = 5000-1 ;
+  htim2.Init.Period = 3500-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
