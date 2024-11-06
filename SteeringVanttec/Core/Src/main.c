258708597248e04bbf5ded6177a114f4b807d718
saucesaft <eduarch42@protonmail.com>
2024-11-05 23:59:01 -0600

    more compatible projects

--- SteeringVanttec/Core/Src/main.c
+++ SteeringVanttec/Core/Src/main.c
@@ -31,6 +31,7 @@
 #include "stepper_task.h"
 #include "stepper.h"
 #include "math.h"
+#include "generic_tasks/generic_tasks.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -119,6 +120,7 @@ void default_task(void *argument);
   */
 int main(void)
 {
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -150,7 +152,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   int filters[] = {0x01A0, 0x0410, 0x0013};
   int filters_size = sizeof(filters) / sizeof(filters[0]);
-  init_canlib(hcan1, VANTTEC_CAN_ID_STEPPER_TX, filters, filters_size);
+  init_canlib(hcan1, VANTTEC_CAN_ID_STEPPER_TX);
   init_canlib_tx();
   init_canlib_rx();
   encoder_setup_can(&hcan1);
@@ -246,6 +248,7 @@ int main(void)
   osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -560,7 +563,7 @@ static void MX_TIM16_Init(void)
   htim16.Instance = TIM16;
   htim16.Init.Prescaler = 80-1;
   htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim16.Init.Period = 1500-1; //Modify value to make the stepper go faster, lower -> faster
+  htim16.Init.Period = 5000-1;
   htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim16.Init.RepetitionCounter = 0;
   htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
--- SteeringVanttec/Core/Src/stm32l4xx_hal_msp.c
+++ SteeringVanttec/Core/Src/stm32l4xx_hal_msp.c
@@ -1,3 +1,4 @@
+
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
@@ -20,7 +21,6 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
@@ -65,6 +65,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
   */
 void HAL_MspInit(void)
 {
+
   /* USER CODE BEGIN MspInit 0 */
 
   /* USER CODE END MspInit 0 */
@@ -128,6 +129,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
   /* USER CODE BEGIN ADC1_MspInit 1 */
 
   /* USER CODE END ADC1_MspInit 1 */
+
   }
 
 }
@@ -192,6 +194,7 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
   /* USER CODE BEGIN CAN1_MspInit 1 */
 
   /* USER CODE END CAN1_MspInit 1 */
+
   }
 
 }
@@ -458,6 +461,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
   /* USER CODE BEGIN USART1_MspInit 1 */
 
   /* USER CODE END USART1_MspInit 1 */
+
   }
 
 }
